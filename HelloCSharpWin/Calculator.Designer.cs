508ca54c2dce0a94c98af4686a14ff967d5d144d
jojieee <jjw3768@gmail.com>
2021-11-30 20:37:02 +0900

    계산기

---
+++ HelloCSharpWin/Calculator.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace HelloCSharpWin
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NumScreen = new System.Windows.Forms.Label();
+            this.NumButton1 = new System.Windows.Forms.Button();
+            this.NumButton2 = new System.Windows.Forms.Button();
+            this.NumButton3 = new System.Windows.Forms.Button();
+            this.NumButton4 = new System.Windows.Forms.Button();
+            this.NumButton5 = new System.Windows.Forms.Button();
+            this.NumButton6 = new System.Windows.Forms.Button();
+            this.NumButton17 = new System.Windows.Forms.Button();
+            this.NumButton8 = new System.Windows.Forms.Button();
+            this.NumButton9 = new System.Windows.Forms.Button();
+            this.NumButton0 = new System.Windows.Forms.Button();
+            this.NumPlus = new System.Windows.Forms.Button();
+            this.NumMinus = new System.Windows.Forms.Button();
+            this.NumTimes = new System.Windows.Forms.Button();
+            this.NumDivide = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.NumClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NumScreen
+            // 
+            this.NumScreen.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.NumScreen.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.NumScreen.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.NumScreen.Location = new System.Drawing.Point(12, 9);
+            this.NumScreen.Name = "NumScreen";
+            this.NumScreen.Size = new System.Drawing.Size(342, 46);
+            this.NumScreen.TabIndex = 2;
+            this.NumScreen.Text = "0";
+            this.NumScreen.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // NumButton1
+            // 
+            this.NumButton1.Location = new System.Drawing.Point(26, 76);
+            this.NumButton1.Name = "NumButton1";
+            this.NumButton1.Size = new System.Drawing.Size(75, 23);
+            this.NumButton1.TabIndex = 3;
+            this.NumButton1.Text = "1";
+            this.NumButton1.UseVisualStyleBackColor = true;
+            this.NumButton1.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // NumButton2
+            // 
+            this.NumButton2.Location = new System.Drawing.Point(107, 76);
+            this.NumButton2.Name = "NumButton2";
+            this.NumButton2.Size = new System.Drawing.Size(75, 23);
+            this.NumButton2.TabIndex = 4;
+            this.NumButton2.Text = "2";
+            this.NumButton2.UseVisualStyleBackColor = true;
+            this.NumButton2.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // NumButton3
+            // 
+            this.NumButton3.Location = new System.Drawing.Point(188, 76);
+            this.NumButton3.Name = "NumButton3";
+            this.NumButton3.Size = new System.Drawing.Size(75, 23);
+            this.NumButton3.TabIndex = 5;
+            this.NumButton3.Text = "3";
+            this.NumButton3.UseVisualStyleBackColor = true;
+            this.NumButton3.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // NumButton4
+            // 
+            this.NumButton4.Location = new System.Drawing.Point(26, 105);
+            this.NumButton4.Name = "NumButton4";
+            this.NumButton4.Size = new System.Drawing.Size(75, 23);
+            this.NumButton4.TabIndex = 6;
+            this.NumButton4.Text = "4";
+            this.NumButton4.UseVisualStyleBackColor = true;
+            this.NumButton4.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // NumButton5
+            // 
+            this.NumButton5.Location = new System.Drawing.Point(107, 105);
+            this.NumButton5.Name = "NumButton5";
+            this.NumButton5.Size = new System.Drawing.Size(75, 23);
+            this.NumButton5.TabIndex = 7;
+            this.NumButton5.Text = "5";
+            this.NumButton5.UseVisualStyleBackColor = true;
+            this.NumButton5.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // NumButton6
+            // 
+            this.NumButton6.Location = new System.Drawing.Point(188, 105);
+            this.NumButton6.Name = "NumButton6";
+            this.NumButton6.Size = new System.Drawing.Size(75, 23);
+            this.NumButton6.TabIndex = 8;
+            this.NumButton6.Text = "6";
+            this.NumButton6.UseVisualStyleBackColor = true;
+            this.NumButton6.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // NumButton17
+            // 
+            this.NumButton17.Location = new System.Drawing.Point(26, 134);
+            this.NumButton17.Name = "NumButton17";
+            this.NumButton17.Size = new System.Drawing.Size(75, 23);
+            this.NumButton17.TabIndex = 9;
+            this.NumButton17.Text = "7";
+            this.NumButton17.UseVisualStyleBackColor = true;
+            this.NumButton17.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // NumButton8
+            // 
+            this.NumButton8.Location = new System.Drawing.Point(107, 134);
+            this.NumButton8.Name = "NumButton8";
+            this.NumButton8.Size = new System.Drawing.Size(75, 23);
+            this.NumButton8.TabIndex = 10;
+            this.NumButton8.Text = "8";
+            this.NumButton8.UseVisualStyleBackColor = true;
+            this.NumButton8.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // NumButton9
+            // 
+            this.NumButton9.Location = new System.Drawing.Point(188, 134);
+            this.NumButton9.Name = "NumButton9";
+            this.NumButton9.Size = new System.Drawing.Size(75, 23);
+            this.NumButton9.TabIndex = 11;
+            this.NumButton9.Text = "9";
+            this.NumButton9.UseVisualStyleBackColor = true;
+            this.NumButton9.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // NumButton0
+            // 
+            this.NumButton0.Location = new System.Drawing.Point(107, 163);
+            this.NumButton0.Name = "NumButton0";
+            this.NumButton0.Size = new System.Drawing.Size(75, 23);
+            this.NumButton0.TabIndex = 12;
+            this.NumButton0.Text = "0";
+            this.NumButton0.UseVisualStyleBackColor = true;
+            this.NumButton0.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // NumPlus
+            // 
+            this.NumPlus.Location = new System.Drawing.Point(269, 76);
+            this.NumPlus.Name = "NumPlus";
+            this.NumPlus.Size = new System.Drawing.Size(75, 23);
+            this.NumPlus.TabIndex = 13;
+            this.NumPlus.Text = "+";
+            this.NumPlus.UseVisualStyleBackColor = true;
+            this.NumPlus.Click += new System.EventHandler(this.NumPlus_Click);
+            // 
+            // NumMinus
+            // 
+            this.NumMinus.Location = new System.Drawing.Point(269, 105);
+            this.NumMinus.Name = "NumMinus";
+            this.NumMinus.Size = new System.Drawing.Size(75, 23);
+            this.NumMinus.TabIndex = 14;
+            this.NumMinus.Text = "-";
+            this.NumMinus.UseVisualStyleBackColor = true;
+            this.NumMinus.Click += new System.EventHandler(this.NumPlus_Click);
+            // 
+            // NumTimes
+            // 
+            this.NumTimes.Location = new System.Drawing.Point(269, 134);
+            this.NumTimes.Name = "NumTimes";
+            this.NumTimes.Size = new System.Drawing.Size(75, 23);
+            this.NumTimes.TabIndex = 15;
+            this.NumTimes.Text = "*";
+            this.NumTimes.UseVisualStyleBackColor = true;
+            this.NumTimes.Click += new System.EventHandler(this.NumPlus_Click);
+            // 
+            // NumDivide
+            // 
+            this.NumDivide.Location = new System.Drawing.Point(269, 163);
+            this.NumDivide.Name = "NumDivide";
+            this.NumDivide.Size = new System.Drawing.Size(75, 23);
+            this.NumDivide.TabIndex = 16;
+            this.NumDivide.Text = "/";
+            this.NumDivide.UseVisualStyleBackColor = true;
+            this.NumDivide.Click += new System.EventHandler(this.NumPlus_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(237, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "=";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.NumPlus_Click);
+            // 
+            // NumClear
+            // 
+            this.NumClear.Location = new System.Drawing.Point(269, 192);
+            this.NumClear.Name = "NumClear";
+            this.NumClear.Size = new System.Drawing.Size(75, 23);
+            this.NumClear.TabIndex = 18;
+            this.NumClear.Text = "C";
+            this.NumClear.UseVisualStyleBackColor = true;
+            this.NumClear.Click += new System.EventHandler(this.NumClear_Click);
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 450);
+            this.Controls.Add(this.NumClear);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.NumDivide);
+            this.Controls.Add(this.NumTimes);
+            this.Controls.Add(this.NumMinus);
+            this.Controls.Add(this.NumPlus);
+            this.Controls.Add(this.NumButton0);
+            this.Controls.Add(this.NumButton9);
+            this.Controls.Add(this.NumButton8);
+            this.Controls.Add(this.NumButton17);
+            this.Controls.Add(this.NumButton6);
+            this.Controls.Add(this.NumButton5);
+            this.Controls.Add(this.NumButton4);
+            this.Controls.Add(this.NumButton3);
+            this.Controls.Add(this.NumButton2);
+            this.Controls.Add(this.NumButton1);
+            this.Controls.Add(this.NumScreen);
+            this.Name = "Calculator";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label NumScreen;
+        private System.Windows.Forms.Button NumButton1;
+        private System.Windows.Forms.Button NumButton2;
+        private System.Windows.Forms.Button NumButton3;
+        private System.Windows.Forms.Button NumButton4;
+        private System.Windows.Forms.Button NumButton5;
+        private System.Windows.Forms.Button NumButton6;
+        private System.Windows.Forms.Button NumButton17;
+        private System.Windows.Forms.Button NumButton8;
+        private System.Windows.Forms.Button NumButton9;
+        private System.Windows.Forms.Button NumButton0;
+        private System.Windows.Forms.Button NumPlus;
+        private System.Windows.Forms.Button NumMinus;
+        private System.Windows.Forms.Button NumTimes;
+        private System.Windows.Forms.Button NumDivide;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button NumClear;
+    }
+}
+
---
+++ HelloCSharpWin/Calculator.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// 툴박스
+namespace HelloCSharpWin
+{
+    // 열겨형 데이터 타입 생성
+    //public enum Season { Spring, Summer, Fall, Winter }
+    public enum Operators { Add, Sub, Multi, Div }
+
+    public partial class Calculator : Form
+    {
+        public int Result = 0;
+        public bool isNewNum = true;
+        public Operators Opt = Operators.Add;
+
+        public Calculator()
+        {
+            InitializeComponent();
+
+            // 열거형 데이터 타입을 할당
+            // Season currentSeason = Season.Spring;
+        }
+
+        // 조건문
+        /*public bool HasValue()
+        {
+            return true;
+        }
+
+        private void HelloLabel_Click(object sender, EventArgs e)
+        {
+            //HelloLabel.Text = "Hello C#";
+
+            // int number1 = 10;
+            // string operator1 = "*";
+            // bool isCorrect = true; // false
+
+            // int sum = 1 + 2;
+
+            int number1 = 1;
+            int number2 = 2;
+            if (HasValue())
+            {
+
+            }
+            else if()
+            {
+
+            }
+
+            int sum = number1 + number2;
+
+            // 정수를 string으로 변환
+            HelloLabel.Text = sum.ToString();
+        }
+
+        private void SumNumbers_Click(object sender, EventArgs e)
+        {
+            // 초기화
+            int number1 = 0;
+            int number2 = 0;
+
+            // 빈문자열, 공백문자열 처리
+            // 벨리데이션
+            if (String.IsNullOrWhiteSpace(Sum1.Text))
+            {
+                // MessageBox Alert창 띄워줌
+                MessageBox.Show("Sum1에 숫자를 입력해주세요");
+                Sum1.Focus();
+                return;
+            }
+
+            // TryParse bool (string형 out int형)
+            // out 값을 함수안에서 받아옴
+            if (int.TryParse(Sum1.Text, out number1) == false)
+            {
+                // MessageBox Alert창 띄워줌
+                MessageBox.Show("Sum1에 문자가 들어왔습니다. 숫자를 입력해주세요");
+                // 전체선택 사용이유: 잘못입력된 값을 바로 수정하기 편하게
+                Sum1.SelectAll();
+                // 포커스 사용이유 : 잘못된 부분으로 이동
+                Sum1.Focus();
+                return;
+            }
+
+            if (String.IsNullOrWhiteSpace(Sum2.Text))
+            {
+                // MessageBox Alert창 띄워줌
+                MessageBox.Show("Sum2에 숫자를 입력해주세요");
+                return;
+            }
+
+            // TryParse bool (string형 out int형)
+            // out 값을 함수안에서 받아옴
+            if (int.TryParse(Sum2.Text, out number2) == false)
+            {
+                // MessageBox Alert창 띄워줌
+                MessageBox.Show("Sum2에 문자가 들어왔습니다. 숫자를 입력해주세요");
+                return;
+            }
+
+            
+
+            int sum = Add(number1, number2);
+            // convert.tostring(sum) 과 같음
+            SumResult.Text = sum.ToString();
+        }
+        */
+
+        // 메서드
+        public int Add(int number1, int number2)
+        {
+            int sum = number1 + number2;
+            return sum;
+        }
+
+        public float Add(float number1, float number2)
+        {
+            // 실수형(1.2, 2.5 ...)
+            float sum = number1 + number2;
+            return sum;
+        }
+
+
+        // public void Flush(Number1);
+        // public void Flush(Number2);
+        // public void Flush(Number1, Number2);
+        // public void Flush();
+
+        // void : 리턴값이 없다.
+
+        // 함수의 오버로딩 : 
+        // 같은 함수의 이름을 갖는것 가능
+        // 하지만 같은 함수 같은 매개변수는 불가능
+
+        public int Sub(int number1, int number2)
+        {
+            int Sub = number1 - number2;
+            return Sub;
+        }
+
+        private void NumButton1_Click(object sender, EventArgs e)
+        {
+            /*
+           if (NumScreen.Text == "0")
+               NumScreen.Text = "1";
+           else
+               NumScreen.Text = NumScreen.Text + "1";
+           */
+            // 모든 버튼에 대한 핸들링
+            Button numButton = (Button)sender;
+            SetNum(numButton.Text);
+        }
+       
+
+        public void SetNum(string num)
+        {
+
+            if (isNewNum)
+            {
+                NumScreen.Text = num;
+                isNewNum = false;
+            }
+            else if (NumScreen.Text == "0")
+            {
+                NumScreen.Text = num;
+            }
+            else
+            {
+                NumScreen.Text = NumScreen.Text + num;
+            }
+            /*
+            if (NumScreen.Text == "0")
+                NumScreen.Text = num;
+            else
+                NumScreen.Text = NumScreen.Text + num;
+            */
+        }
+
+        private void NumPlus_Click(object sender, EventArgs e)
+        {
+            // 예외처리 연산자 연속 누르기 방지
+            if(isNewNum == false)
+            {
+                // string 을 int로 변환
+                int num = int.Parse(NumScreen.Text);
+                if (Opt == Operators.Add)
+                    // 더하기 버튼을 누르면 현재값 변경
+                    Result = Add(Result, num);
+                else if (Opt == Operators.Sub)
+                    Result = Sub(Result, num);
+                else if (Opt == Operators.Multi)
+                    Result = Result * num;
+                else if (Opt == Operators.Div)
+                    Result = Result / num;
+
+                // NumScreen에 표시
+                NumScreen.Text = Result.ToString();
+                // 새로운 숫자를 받아라
+                isNewNum = true;
+            }
+
+            Button optButton = (Button)sender;
+            if (optButton.Text == "+")
+                Opt = Operators.Add;
+            else if (optButton.Text == "-")
+                Opt = Operators.Sub;
+            else if (optButton.Text == "*")
+                Opt = Operators.Multi;
+            else if (optButton.Text == "/")
+                Opt = Operators.Div;
+        }
+
+        private void NumClear_Click(object sender, EventArgs e)
+        {
+            Result = 0;
+            isNewNum = true;
+            Opt = Operators.Add;
+
+            NumScreen.Text = "0";
+        }
+
+        // 변수 = 0
+        // 연산자 = +
+
+        // 숫자입력
+        // 연산자버튼 - 숫자완성, 변수와 숫자를 저장된 연산자로 연산, 결과를 변수에 저장, 현재 연산자를 저장
+        // 숫자입력
+        // 연산자버튼 - 숫자완성, 변수와 숫자를 저장된 연산자로 연산, 결과를 변수에 저장, 현재 연산자를 저장
+    }
+}
